20ef485531e35cb17f82b5078749a2d6d9f3b37c
MyManny <gmail.com>
2023-10-05 20:14:50 -0700

    update slice

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -1,9 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
-
 const initialState = {
-    objectId: 5500,
+    objectId: 14200,
     apiData: {}
 }
 
@@ -11,10 +9,23 @@ export const dataSlice = createSlice({
     name: 'data',
     initialState,
     reducers: {
-        setData: (state, action) => ({ ...state, apiData: action.payload}),
-        incrementId: () => {},
-        decrementId: () => {},
-        customId: () => {},
-        clearData: () => {}
+        setData: (state, action) => ({ ...state, apiData: action.payload }),
+        incrementId: (state) => ({ ...state, objectId: state.objectId + 1 }),
+        decrementId: (state) => ({ ...state, objectId: state.objectId - 1 }),
+        customId: (state, action) => ({ ...state, objectId: action.payload }),
+        clearData: () => initialState
     }
 })
+
+export const fetchData = () => {
+    const dataThunk = async (dispatch, getState) => {
+        const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${getState().data.objectId}`)
+        const resData = await data.json()
+        dispatch(setData(resData))
+    }
+    return dataThunk
+}
+
+export const { setData, incrementId, decrementId, customId, clearData } = dataSlice.actions
+
+export default dataSlice.reducer
